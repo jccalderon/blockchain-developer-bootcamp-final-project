be45d725516f48a65f5f04c912eb403ee73b0bd7
Juan Calderon <jccalderonm@gmail.com>
2021-11-16 21:42:24 -0500

    scaffolding charlize first compilation succ

--- contracts/Charlize.sol
+++ contracts/Charlize.sol
@@ -28,7 +28,7 @@ contract Charlize is KeeperCompatibleInterface{
 
     /// @param  companyName, role, email 
     /// @return boolean true if successfully registered, false if registration failed.
-    function registerUser(string _companyName, Role _role, string _email) public returns(bool){
+    function registerUser(string memory companyName, Role role, string memory email) public returns(bool){
 
     }
 
@@ -43,16 +43,16 @@ contract Charlize is KeeperCompatibleInterface{
     ///     Reagent's data is stored in a mapping (companyId => User)
     /// @param NDC, lotNumber, expirationDate
     /// @return boolean true if successfully created, false if creation failed.
-    function createReagent(uint256 _NDC, uint256 lotNumber, string expirationDate) public returns(bool){
+    function createReagent(uint256 NDC, uint256 lotNumber, string memory expirationDate) public returns(bool){
 
     }
 
 
-    /// @notice gets data about a particular Reagent
+    /// @notice gets data about a particular reagent
     /// @dev looks up data of a particular reagent
     /// @param NDC, lotNumber
-    /// @return string cointaining Reagent.State() and expirationDate
-    function getReagentData(uint256 _NDC, uint256 lotNumber ) public returns(string expirationDate){
+    /// @return expirationDate
+    function getReagentData(uint256 NDC, uint256 lotNumber ) public returns(string memory expirationDate){
 
     }
   
@@ -70,13 +70,13 @@ contract Charlize is KeeperCompatibleInterface{
 
     /// @param checkData (not used in this case)
     /// @return upkeepNeeded (if true the keeper executes the performUpkeep function)
-    function checkUpkeep(bytes calldata /* checkData */) external override returns (bool upkeepNeeded, bytes memory /* performData */) {
+    function checkUpkeep(bytes calldata checkData) external override returns (bool upkeepNeeded, bytes memory /* performData */) {
        
         
     }
 
     /// @param performData (not used in this case)
-    function performUpkeep(bytes calldata /* performData */) external override {
+    function performUpkeep(bytes calldata performData) external override {
 
         
     }   
